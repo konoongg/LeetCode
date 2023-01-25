5295fea2aa1ce85bff5457545efa33ffca11a059
konoongg <120199924+konoongg@users.noreply.github.com>
2023-01-26 04:12:38 +0700

    add des

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -11,4 +11,12 @@ class Solution:
             else:
                 ms.append(str(i))
         return ms
-        
+''' 
+    Given an integer n, return a string array answer (1-indexed) where:
+
+    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    answer[i] == "Fizz" if i is divisible by 3.
+    answer[i] == "Buzz" if i is divisible by 5.
+    answer[i] == i (as a string) if none of the above conditions are true.
+    https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/743/
+'''
--- PowerOfThree.py
+++ PowerOfThree.py
@@ -7,4 +7,6 @@ class Solution:
                 return False;
             n //= 3
         return True
-        
+#Given an integer n, return true if it is a power of three. Otherwise, return false.
+#An integer n is a power of three, if there exists an integer x such that n == 3x.
+#https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/745/
