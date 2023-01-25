d1758d8ba59fac06d1f07cb609322c8897acf938
konoongg <120199924+konoongg@users.noreply.github.com>
2023-01-26 04:42:14 +0700

    add file and create folder for math

---
+++ math/RomanToInteger.py
@@ -0,0 +1,48 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sum = 0
+        lastNum = 0
+        for i in range(len(s) - 1 , -1, -1):
+            num = 0
+            if s[i] == 'I':
+                num = 1
+            elif s[i] == 'V':
+                num = 5
+            elif s[i] == 'X':
+                num = 10
+            elif s[i] == 'L':
+                num = 50
+            elif s[i] == 'C':
+                num = 100
+            elif s[i] == 'D':
+                num = 500
+            else:
+                num = 1000
+            if num >= lastNum:
+                sum += num
+            else:
+                sum -= num
+            lastNum = num
+        return sum
+
+'''
+    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+    Symbol       Value
+    I             1
+    V             5
+    X             10
+    L             50
+    C             100
+    D             500
+    M             1000
+    For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+    Given a roman numeral, convert it to an integer.
+    https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/878/
+ '''
