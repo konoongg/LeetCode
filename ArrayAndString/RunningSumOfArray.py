3422b86f291c6f1525f916c010bc80df4fb5ae60
konoongg <120199924+konoongg@users.noreply.github.com>
2023-02-02 17:13:38 +0700

    Update RunningSumOfArray.py

--- ArrayAndString/RunningSumOfArray.py
+++ ArrayAndString/RunningSumOfArray.py
@@ -4,8 +4,4 @@ class Solution:
             nums[i] += nums[i - 1]
         return nums 
 
-"""
-https://leetcode.com/problems/running-sum-of-1d-array/description/
-Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
-Return the running sum of nums.
-"""
+
