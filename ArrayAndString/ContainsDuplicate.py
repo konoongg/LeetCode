19b0db6d2eae66d3badb5b035f5e95d67cd8996d
konoongg <120199924+konoongg@users.noreply.github.com>
2023-02-02 17:10:35 +0700

    Update ContainsDuplicate.py

--- ArrayAndString/ContainsDuplicate.py
+++ ArrayAndString/ContainsDuplicate.py
@@ -18,7 +18,4 @@ class Solution:
                 return True
         return False
      """
-"""
-https://leetcode.com/problems/contains-duplicate/description/
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-"""
+
